e957c4bf8a6b2c59781b18ff740e7227eef204d7
Bikesh Rana <bksh.rana@hotmail.com>
2019-05-27 22:29:08 +0100

    Add project files.

---
+++ InterestRatesTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InterestRatesTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
